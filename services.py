f57e338ff9fa93fd62d8ff75b6b0f816b643eedc
Hampus Ram <hampus.ram@gmail.com>
2026-02-07 11:34:51 +0100

    new web service for prices

--- services.py
+++ services.py
@@ -1,10 +1,10 @@
 import locale
 import logging
 import requests
-import time
 
 from datetime import datetime
 from statistics import mean
+from time import sleep
 from typing import TypeVar, Callable, Any, Dict
 
 T = TypeVar("T")
@@ -21,13 +21,13 @@ def retry(data_func: Callable[[], T]) -> T:
     for i in range(0, 10):
         try:
             res = data_func()
-            if res != None:
+            if res is not None:
                 return res
         except Exception:
             log.exception("retrieval raised an error")
         
         log.info(f"waiting to retry for {retry_time_seconds} seconds")
-        time.sleep(retry_time_seconds)
+        sleep(retry_time_seconds)
         retry_time_seconds = retry_time_seconds * 2
     
     raise RuntimeError("retry failed")
@@ -56,21 +56,27 @@ def todays_electrical_prices() -> Dict[str, Any]:
     """Get todays electical prices for region SE3."""
     log.info("getting todays electricity prices")
 
-    today = datetime.strftime(datetime.today(), "%Y-%m-%d")
-    headers={
+    start_of_day = datetime.combine(datetime.today(), time.min)
+    sod_ts = int(start_of_day.timestamp())
+    end_of_day = datetime.combine(datetime.today(), time.max)
+    eod_ts = int(end_of_day.timestamp())
+
+    headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.41",
-        "accept": "application/json,text/html;q=0.99,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+        "accept": "application/json,text/html;q=0.99,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     }
 
-    spot_price_url = f"https://www.vattenfall.se/api/price/spot/pricearea/{today}/{today}/SN3"
+    spot_price_url = f"https://selfserviceapi.www.vattenfall.se/elements/nordpool/aggregatedspotprices?deliveryAreas=SE3&currency=SEK&deliveryStart={sod_ts}&deliveryEnd={eod_ts}&resolution=15mins&timezone=CET"
     response = requests.get(spot_price_url, headers=headers)
 
     if response.status_code >= 400:
         raise RuntimeError("unable to get page")
 
-    prices = [ val['Value'] for val in response.json() ]
+    json = response.json()
+    prices = [val["measurement"]["value"] for val in json["prices"]]
+    prices_per_hour = [max(prices[i : i + 4]) for i in range(0, len(prices), 4)]
 
-    return { "min": min(prices), "mean": mean(prices), "max": max(prices), "all": prices }
+    return {"min": min(prices_per_hour), "mean": mean(prices_per_hour), "max": max(prices_per_hour), "all": prices_per_hour}
 
 def _days_from_today(time_string: str) -> int:
     parsed_time = datetime.strptime(time_string, "%d %B, %Y")
